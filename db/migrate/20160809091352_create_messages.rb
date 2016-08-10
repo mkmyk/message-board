f79295dafbefdb14e2ab1ce9e1a847dbf2a0fe6f
mkmyk <myykn78@gmail.com>
2016-08-10 01:44:38 +0000

    message model comment added in migration file

--- db/migrate/20160809091352_create_messages.rb
+++ db/migrate/20160809091352_create_messages.rb
@@ -1,9 +1,17 @@
+# a migration file create and change  a SQL database
+# rake db:migrate command writes a scheme in db:schema.rb
+# rake db:migrate command also creates a table in a SQL database
+
 class CreateMessages < ActiveRecord::Migration
   def change
+
+    # create_table method creates a messages table
     create_table :messages do |t|
       t.string :name
       t.string :body
 
+      # records when a record is created, or updated
+      # null is not allowed
       t.timestamps null: false
     end
   end
