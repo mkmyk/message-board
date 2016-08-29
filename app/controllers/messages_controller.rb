fa5917ba05baacdaf1d26fb5bb6e939fbcdbd9f4
mkmyk <myykn78@gmail.com>
2016-08-29 04:57:38 +0000

    using Bootstrap input area

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,5 @@
 class MessagesController < ApplicationController
-  #MessageController has many actions　(methods).
+  before_action :set_message, only: [:edit, :update,:delete]
 
   #This call the view "app/views/messages/index.html.erb"
   def index
@@ -24,9 +24,31 @@ class MessagesController < ApplicationController
     end
   end
   
+  def edit
+  end
+  
+  def update
+    if @message.update(message_params)
+      #保存に成功した場合はトップページへリダイレクト
+      redirect_to root_path, notice: 'メッセージを編集しました。'
+    else
+      #保存に失敗した場合は編集画面へ戻す
+      render 'edit'
+    end
+  end
+  
+  def destroy
+    @message.destroy
+    redirect_to root_path, notice: 'メッセージを削除しました'
+  end
+  
   private 
   def message_params
     params.require(:message).permit(:name, :body)
+  end
+
+  def set_message
+    @mesasge=Message.find(params[:id])
   end
   
 end
